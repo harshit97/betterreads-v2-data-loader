4c394cd19657dfb7a8693af81a02a42d1446d78b
harshagarwal <harshagarwal@paypal.com>
2022-03-26 11:50:21 +0530

    Initial connection setup with Datastax Cassandra

--- src/main/java/io/harshit/betterreadsv2dataloader/BetterreadsV2DataLoaderApplication.java
+++ src/main/java/io/harshit/betterreadsv2dataloader/BetterreadsV2DataLoaderApplication.java
@@ -1,13 +1,25 @@
 package io.harshit.betterreadsv2dataloader;
 
+import io.harshit.betterreadsv2dataloader.connection.DataStaxAstraProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.cassandra.CqlSessionBuilderCustomizer;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+
+import java.nio.file.Path;
 
 @SpringBootApplication
+@EnableConfigurationProperties(DataStaxAstraProperties.class)
 public class BetterreadsV2DataLoaderApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BetterreadsV2DataLoaderApplication.class, args);
 	}
 
+    @Bean
+    public CqlSessionBuilderCustomizer sessionBuilderCustomizer(DataStaxAstraProperties dataStaxAstraProperties) {
+        Path bundle = dataStaxAstraProperties.getSecureConnectBundle().toPath();
+        return builder -> builder.withCloudSecureConnectBundle(bundle);
+    }
 }
