8ed5cb7d3172cd71137ae05faa80d5913a885ef9
harshagarwal <harshagarwal@paypal.com>
2022-03-26 10:31:39 +0530

    Intial Spring setup commit

---
+++ src/main/java/io/harshit/betterreadsv2dataloader/BetterreadsV2DataLoaderApplication.java
@@ -0,0 +1,13 @@
+package io.harshit.betterreadsv2dataloader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetterreadsV2DataLoaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BetterreadsV2DataLoaderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/harshit/betterreadsv2dataloader/BetterreadsV2DataLoaderApplicationTests.java
@@ -0,0 +1,13 @@
+package io.harshit.betterreadsv2dataloader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BetterreadsV2DataLoaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
