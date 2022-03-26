c96f25c3365ab54f385b61eba0ef5afa424111ed
harshagarwal <harshagarwal@paypal.com>
2022-03-26 12:59:51 +0530

    Separating out CQL session

--- src/main/java/io/harshit/betterreadsv2dataloader/BetterreadsV2DataLoaderApplication.java
+++ src/main/java/io/harshit/betterreadsv2dataloader/BetterreadsV2DataLoaderApplication.java
@@ -1,25 +1,34 @@
 package io.harshit.betterreadsv2dataloader;
 
+import io.harshit.betterreadsv2dataloader.author.AuthorLoader;
 import io.harshit.betterreadsv2dataloader.connection.DataStaxAstraProperties;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.cassandra.CqlSessionBuilderCustomizer;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 
+import javax.annotation.PostConstruct;
 import java.nio.file.Path;
 
 @SpringBootApplication
 @EnableConfigurationProperties(DataStaxAstraProperties.class)
 public class BetterreadsV2DataLoaderApplication {
 
+    @Autowired
+    private AuthorLoader authorLoader;
+
 	public static void main(String[] args) {
 		SpringApplication.run(BetterreadsV2DataLoaderApplication.class, args);
 	}
 
-    @Bean
-    public CqlSessionBuilderCustomizer sessionBuilderCustomizer(DataStaxAstraProperties dataStaxAstraProperties) {
-        Path bundle = dataStaxAstraProperties.getSecureConnectBundle().toPath();
-        return builder -> builder.withCloudSecureConnectBundle(bundle);
+    @PostConstruct
+    public void start() {
+        authorLoader.loadAuthors();
+        //initAuthors();
+        //initWorks();
     }
+
+
 }
